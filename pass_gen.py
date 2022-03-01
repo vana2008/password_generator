9161e366cb261d4b17b31e774d012b5ff644941d
vana2008 <92047848+vana2008@users.noreply.github.com>
2022-03-01 19:24:21 +0500

    Update pass_gen.py

--- pass_gen.py
+++ pass_gen.py
@@ -1,10 +1,10 @@
-import random
-lower = "qwertyuiopasdfghjklzxcvbnm" 
-upper = "QWERTYUIOPASDFGHJKLZXCVBNM"
-NUMBER = "0123456789"
-Symbol = "[]{}#()*;._-"
+import random #pip install random
+lower = "qwertyuiopasdfghjklzxcvbnm" #маленькие буквы в пароле
+upper = "QWERTYUIOPASDFGHJKLZXCVBNM" #большие буквы в пароле
+NUMBER = "0123456789" # цифры
+Symbol = "[]{}#()*;._-" # символы
 
-all=lower+upper+NUMBER+Symbol
-lenght = 15
-password = "".join(random.sample(all, lenght))
-print("Sosi hui i ne psihui :", password)
+all=lower+upper+NUMBER+Symbol #из чего будет состоять пароль
+lenght = 15 #длина пароля. можно поставить свое число, какое захотите
+password = "".join(random.sample(all, lenght)) #переменная password. и из чего он будет состоять
+print("Sosi hui i ne psihui :", password) #вывод сообщения с паролем в терминал
