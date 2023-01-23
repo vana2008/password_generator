8fe4d92d19fac4670bcee0eb59de5a3002b82c1b
vana2008 <92047848+vana2008@users.noreply.github.com>
2023-01-23 21:29:17 +0500

    Update pass_gen.py

--- pass_gen.py
+++ pass_gen.py
@@ -7,4 +7,4 @@ Symbol = "[]{}#()*;._-" # символы
 all=lower+upper+NUMBER+Symbol #из чего будет состоять пароль
 lenght = 15 #длина пароля. можно поставить свое число, какое захотите
 password = "".join(random.sample(all, lenght)) #переменная password. и из чего он будет состоять
-print("Sosi hui i ne psihui :", password) #вывод сообщения с паролем в терминал
+print("pswrd :", password) #вывод сообщения с паролем в терминал
